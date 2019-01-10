daa06cae6880b60314499d969c6ebef98965ef34
Phuoc Do <binanhphuoc@gmail.com>
2019-01-10 00:26:13 -0600

    display tex result

--- src/libs/wolfram/parser.js
+++ src/libs/wolfram/parser.js
@@ -1,5 +1,16 @@
-const parser = (arrayString) => {
+import axios from 'axios';
+
+export const JSONparser = (arrayString) => {
     return JSON.parse(JSON.parse(arrayString));
+};
+
+export const TeXparser = async (stdString) => {
+    let result = await axios.get("https://www.wolframcloud.com/objects/4d6d5550-5b10-4daf-ae17-faed628f04c8",{
+        params: {
+            stdform: stdString,
+        }
+    });
+    return JSON.parse(result.data.Result);
 }
 
-export default parser;
+export default {JSONparser, TeXparser}
--- src/libs/wolfram/solver.js
+++ src/libs/wolfram/solver.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import parser from './parser'
+import {JSONparser} from './parser'
 
 const logicProof = async (problem) => {
     var result = await axios.get("https://www.wolframcloud.com/objects/3408893e-452a-49cc-bbbe-2154fd636900",{
@@ -7,7 +7,7 @@ const logicProof = async (problem) => {
             problem
         }
     })
-    return parser(result.data.Result);
+    return JSONparser(result.data.Result);
 }
 
 export default logicProof;
