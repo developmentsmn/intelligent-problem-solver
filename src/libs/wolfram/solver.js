4367b6199ab60758dded00d35c8a09a71b258a1b
Brandon <brandon.carter3@mavs.uta.edu>
2019-03-04 06:11:44 -0600

    Propositional Logic problems now follow the format specified in the text field as well as use the JSON structure to grab the wolfram links

--- src/libs/wolfram/solver.js
+++ src/libs/wolfram/solver.js
@@ -2,7 +2,28 @@ import axios from "axios";
 import { JSONparser } from "./parser";
 
 const logicProof = async (problem) => {
-  const result = await axios.get("https://www.wolframcloud.com/objects/405cf2ed-d64a-4a43-9a84-9d194517b783", {
+  console.log(problem);
+  const terms = problem.split("#");
+
+  var linkData = require('./solverLinks.json')
+
+  var link = undefined;
+  //console.log("array:", terms);
+
+  if(terms[1] === "Propositional Logic ")
+  {
+    const key = "PropositionalLogic";
+    if(terms[2] === "Prove ")
+    {
+      const key2 = "Prove";
+      link = linkData[key][key2]
+      console.log("link:", link);
+      problem = terms[3];
+    }
+  }
+
+  //https://www.wolframcloud.com/objects/405cf2ed-d64a-4a43-9a84-9d194517b783
+  const result = await axios.get(link, {
     params: {
       problem,
     },
